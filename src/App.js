efdafcca9ec70c44d5a3602fb0ae0f9f1b1d94de
demetcaliskan <demetcaliskann@gmail.com>
2021-03-20 03:56:18 +0300

    Added Navbar with FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar />
       </div>
     );
   }
