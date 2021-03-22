6f333a57592d6f085c53bb84da8f5b9f8b7b9d8d
demetcaliskan <demetcaliskann@gmail.com>
2021-03-22 03:51:51 +0300

    search is actively functioning

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
 import axios from 'axios'
+import PropTypes from 'prop-types'
 import './App.css';
 
 class App extends Component {
@@ -12,19 +13,18 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
-
-    this.setState({ loading: true });
-
-    const res = await axios.get(`https://api.github.com/users?client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({ users: res.data, loading: false })
-
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
   }
 
-  searchUsers = (text) => {
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false });
 
   }
 
